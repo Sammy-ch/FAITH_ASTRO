c335d20df378851be77c396795d90e2ac5a68359
Sammy Saint <sammystcherubin@gmail.com>
2023-09-14 15:05:46 +0200

    Added Stagger Animation on KeyFeatures Section

--- src/layouts/function-components/HomapageFeature.jsx
+++ src/layouts/function-components/HomapageFeature.jsx
@@ -1,11 +1,37 @@
+import {motion} from "framer-motion"
+
+
+
+
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate:(i) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay:0.5 * i
+    }
+  }),
+};
+
 
 const HomapageFeature = ({ feature_list }) => {
   return (
     <div className="key-feature-grid text-center mt-10 grid grid-cols-2 gap-7 md:grid-cols-3 xl:grid-cols-4">
       {feature_list.map((item, i) => {
         return (
-          <div
+          <motion.div
             key={i}
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once:true,
+            }}
+            custom={i}
             className="flex flex-col justify-between rounded-lg bg-white p-5 shadow-lg"
           >
             <div>
@@ -14,7 +40,7 @@ const HomapageFeature = ({ feature_list }) => {
               <p className="text-center">{item.content}</p>
             </div>
             
-          </div>
+          </motion.div>
         );
       })}
     </div>
